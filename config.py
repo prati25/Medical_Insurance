ef9ef594b809e698bd2d933f57fb2b0d081bf456
prati25 <pratiksha.khedkar2016@gmail.com>
2024-06-12 04:44:42 -0700

    add file

---
+++ config.py
@@ -0,0 +1,6 @@
+MODEL_FILE_PATH = r'project_app\Linear_Model.pkl'
+JSON_FILE_PATH = r'project_app\project_data.json'
+
+PORT_NUMBER = 5005
+#MODEL_FILE_PATH = 'Linear_Model.pkl'
+#JSON_FILE_PATH = 'project_data.json'
